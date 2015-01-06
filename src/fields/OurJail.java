4d4b05916715677edbf3163e7aea42928dd4ae91
Mathias Sondrup <matsondrup@gmail.com>
2015-01-06 10:27:34 +0100

    Added OurChance.java and OurChanceController.java

--- src/fields/OurJail.java
+++ src/fields/OurJail.java
@@ -1,5 +1,9 @@
 package fields;
 
 public class OurJail extends OurField{
-
+	public OurJail(String name, int fieldId, String type){
+		super.setName(name);
+		super.setFieldId(fieldId);
+		super.setType(type);
+	}
 }
