e188f7c5e213ca5e86a9b540c154eb3c2fe38057
Mathias Sondrup <matsondrup@gmail.com>
2015-01-06 11:15:20 +0100

    Added numberOfHouses to OurStreet.

--- src/fields/OurStreet.java
+++ src/fields/OurStreet.java
@@ -6,10 +6,11 @@ public class OurStreet extends Ownable{
 	private int[] rent = new int[6];
 	private int housePrice;
 	private Color color;
+	private int numberOfHouses;
 	
 	public OurStreet(int[] rent, int price, int housePrice, String name, int id, int fieldPossition){
 		this.rent = rent;
-		this.housePrice = housePrice;
+		this.setHousePrice(housePrice);
 		super.setPrice(price);
 		super.setName(name);
 		super.setFieldId(id);
@@ -29,11 +30,27 @@ public class OurStreet extends Ownable{
 	
 	@Override
 	public int getRent() {
-		return rent;
+		return rent[numberOfHouses];
 	}
 	
 	public String toString() {
 		String s = "Rent: " + rent + " Price: " + super.getPrice() + " Name: " + super.getOwner().getName() + " fieldId: " + super.getFieldId();
 		return s;
+	}
+
+	public int getHousePrice() {
+		return housePrice;
+	}
+
+	public void setHousePrice(int housePrice) {
+		this.housePrice = housePrice;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
 	}
 }
