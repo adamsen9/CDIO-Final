c7ce0e1e9f046dced2e949a448aa48cf757ae753
Adamsen <adamsen9@gmail.com>
2015-01-07 10:31:19 +0100

    Auctioncontroller created

---
+++ src/controllers/AuctionController.java
@@ -0,0 +1,27 @@
+package controllers;
+import entities.*;
+import fields.*;
+
+
+public class AuctionController {
+
+	public AuctionController(){
+		
+	}
+	
+	
+	public boolean bankruptcyAuction(){
+		
+		
+		return true;
+	}
+	
+	public boolean Auction(GUIManager GUI, Player players[], Ownable field){
+		
+		
+		return true;
+	}
+	
+	
+	
+}
