aa184f355803f61b5947b48008e467b8a7a44f55
leschly <victorleschlyrnby@10.16.130.39>
2015-01-08 13:35:17 +0100

    Har arbejdet på Chance-felterne

--- src/controllers/OurChanceController.java
+++ src/controllers/OurChanceController.java
@@ -8,19 +8,42 @@ import fields.OurField;
 import entities.OurChanceCard;
 
 
-public class OurChanceController extends FieldController{
-	OurChanceCard[] liste = new OurChanceCard[5];
-public OurChanceController() {
+	public class OurChanceController extends FieldController{
+		OurChanceCard[] liste = new OurChanceCard[5];
+		private Random rnd = new Random();
+		int FaceValue;
+		
+	public OurChanceController() {
+		liste[0] = new OurChanceCard("Ryk 2 felter tilbage", "betal", 1000);
+		//liste[1] = new OurChanceCard("jeg er kort nr 2","ryk", 2);
+		//liste[2] = new OurChanceCard("jeg er kort nr 3", "betal", 3000);
+		//liste[3] = new OurChanceCard("jeg er kort nr 4", "ryk", 4);
+		//liste[4] = new OurChanceCard("jeg er kort nr 5", "betal", 5000);	
+	}
 	
-	
+	public void putCardOnStack(GUIManager display){
+		FaceValue = (int) rnd.nextInt(1);
+		display.setcard(liste[FaceValue].getText() + ". " +  liste[FaceValue].getEffect() +  " " + liste[FaceValue].getAmount());
 		
 	}
+	
 	
 	@Override
 	public boolean landOnField(Player player, GUIManager display,
 			OurField field, Die die) {
-		// TODO Auto-generated method stub
-	
+		switch (liste[FaceValue].getText()) {
+			case ("Ryk 2 felter tilbage"):
+			display.sendMessage(liste[FaceValue].getText());
+			display.movePlayer(field.getFieldId(), field.getFieldId()-2, player.getName());
+			player.move(-2);
+			
+			
+			break;
+			
+			
+
+
+		}
 		
 		return false;
 	}
